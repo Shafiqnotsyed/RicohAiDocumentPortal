19ee297b89101039671254a643f3f4aff9244247
shafiqnotsyed <shafiqsa427@gmail.com>
2026-03-30 13:02:19 +0300

    Layout, profile and homepage updates

---
+++ RicohAiDocumentPortal/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RicohAiDocumentPortal.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
